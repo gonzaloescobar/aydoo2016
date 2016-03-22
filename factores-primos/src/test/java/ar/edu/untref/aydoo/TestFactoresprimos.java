d8bc7a37e8e5f93d427dc3f037ba9cfbd308df3a
Gonza <g.d.escobar88@gmail.com>
2016-03-22 20:57:47 -0300

    Trabajo Practico factores primos

---
+++ factores-primos/src/test/java/ar/edu/untref/aydoo/TestFactoresprimos.java
@@ -0,0 +1,48 @@
+package ar.edu.untref.aydoo;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class TestFactoresprimos {
+	
+	/*Prueba la factorización con el numero 90*/
+	@Test
+	public void testFactorizarNumero90() {
+		
+		FactoresPrimos factoresPrimos = new FactoresPrimos();
+		String resultado = factoresPrimos.descomponerFactoresPrimos(90);
+		
+		Assert.assertEquals("2 3 3 5 ", resultado);
+	}
+	
+	/*Prueba la factorización con el numero 360*/
+	@Test
+	public void testFactorizarNumero360() {
+		
+		FactoresPrimos factoresPrimos = new FactoresPrimos();
+		String resultado = factoresPrimos.descomponerFactoresPrimos(360);
+		
+		Assert.assertEquals("2 2 2 3 3 5 ", resultado);
+	}
+	
+	/*Prueba la factorización con el primer divisor*/
+	@Test
+	public void testFactorizarNumero2() {
+		
+		FactoresPrimos factoresPrimos = new FactoresPrimos();
+		String resultado = factoresPrimos.descomponerFactoresPrimos(2);
+		
+		Assert.assertEquals("2 ", resultado);
+	}
+	
+	/*Prueba la factorización con un numero que no se puede descomponer*/
+	@Test
+	public void testFactorizarNumero7() {
+		
+		FactoresPrimos factoresPrimos = new FactoresPrimos();
+		String resultado = factoresPrimos.descomponerFactoresPrimos(7);
+		
+		Assert.assertEquals("7 ", resultado);
+	}
+
+}
