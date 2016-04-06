969d99025903123bc4e6e37d00cbc36f6712b7e0
Gonza <g.d.escobar88@gmail.com>
2016-04-05 23:43:07 -0300

    nueva funcionalidad: candidatoMayorVotadoANivelNacional

--- ejercicio_votacion/src/test/java/ar/edu/untref/aydoo/IntegracionTest.java
+++ ejercicio_votacion/src/test/java/ar/edu/untref/aydoo/IntegracionTest.java
@@ -5,8 +5,6 @@ import org.junit.Test;
 
 public class IntegracionTest {
 
-
-
 	@Test
 	public void votar() {
 		CentroDeComputos centroDeComputos = new CentroDeComputos();
@@ -28,7 +26,7 @@ public class IntegracionTest {
 
 		Assert.assertEquals(3, unCandidato.getCantidadDeVotosObtenidos());
 	}
-	
+
 	@Test
 	public void partidoConMasVotosPorProvincia() {
 		CentroDeComputos centroDeComputos = new CentroDeComputos();
@@ -53,5 +51,89 @@ public class IntegracionTest {
 		Assert.assertEquals(unPartido, centroDeComputos.getPartidoConMasVotosPorProvincia(unaProvincia));
 	}
 
-	
+	@Test
+	public void partidoConMasVotosPorProvinciaConDosCandidatosDeUnPartido() {
+		CentroDeComputos centroDeComputos = new CentroDeComputos();
+		Provincia unaProvincia = new Provincia("Buenos Aires");
+		Partido unPartido = new Partido("FPV");
+		Partido otroPartido = new Partido("FR");
+		Candidato unCandidato = new Candidato("Scioli");
+		Candidato otroCandidato = new Candidato("Massa");
+		Candidato otroCandidatoFR = new Candidato("De la Sota");
+		Voto unVoto = new Voto(unCandidato, unaProvincia);
+		Voto otroVotoMas = new Voto(otroCandidatoFR, unaProvincia);
+		Voto otroVoto = new Voto(otroCandidato, unaProvincia);
+
+		unPartido.agregarCandidato(unCandidato);
+		otroPartido.agregarCandidato(otroCandidato);
+		otroPartido.agregarCandidato(otroCandidatoFR);
+		unaProvincia.agregarPartido(unPartido);
+		unaProvincia.agregarPartido(otroPartido);
+		centroDeComputos.agregarProvincia(unaProvincia);
+		centroDeComputos.votar(unVoto);
+		centroDeComputos.votar(otroVotoMas);
+		centroDeComputos.votar(otroVoto);
+		centroDeComputos.votar(unVoto);
+		centroDeComputos.votar(unVoto);
+
+		Assert.assertEquals(unPartido, centroDeComputos.getPartidoConMasVotosPorProvincia(unaProvincia));
+	}
+
+	@Test
+	public void partidoConMasVotosPorProvinciaConDosCandidatosDeUnPartidoGanando() {
+		CentroDeComputos centroDeComputos = new CentroDeComputos();
+		Provincia unaProvincia = new Provincia("Buenos Aires");
+		Partido unPartido = new Partido("FPV");
+		Partido otroPartido = new Partido("FR");
+		Candidato unCandidato = new Candidato("Scioli");
+		Candidato otroCandidato = new Candidato("Massa");
+		Candidato otroCandidatoFR = new Candidato("De la Sota");
+		Voto unVoto = new Voto(unCandidato, unaProvincia);
+		Voto otroVotoMas = new Voto(otroCandidatoFR, unaProvincia);
+		Voto otroVoto = new Voto(otroCandidato, unaProvincia);
+
+		unPartido.agregarCandidato(unCandidato);
+		otroPartido.agregarCandidato(otroCandidato);
+		otroPartido.agregarCandidato(otroCandidatoFR);
+		unaProvincia.agregarPartido(unPartido);
+		unaProvincia.agregarPartido(otroPartido);
+		centroDeComputos.agregarProvincia(unaProvincia);
+		centroDeComputos.votar(unVoto);
+		centroDeComputos.votar(otroVotoMas);
+		centroDeComputos.votar(otroVoto);
+		centroDeComputos.votar(otroVoto);
+		centroDeComputos.votar(otroVotoMas);
+		centroDeComputos.votar(otroVoto);
+
+		Assert.assertEquals(otroPartido, centroDeComputos.getPartidoConMasVotosPorProvincia(unaProvincia));
+	}
+
+	@Test
+	public void candidatoConMasVotosANivelNacional() {
+		CentroDeComputos centroDeComputos = new CentroDeComputos();
+		Provincia unaProvincia = new Provincia("Buenos Aires");
+		Partido unPartido = new Partido("FPV");
+		Partido otroPartido = new Partido("FR");
+		Candidato unCandidato = new Candidato("Scioli");
+		Candidato otroCandidato = new Candidato("Massa");
+		Candidato otroCandidatoFR = new Candidato("De la Sota");
+		Voto unVoto = new Voto(unCandidato, unaProvincia);
+		Voto otroVotoMas = new Voto(otroCandidatoFR, unaProvincia);
+		Voto otroVoto = new Voto(otroCandidato, unaProvincia);
+
+		unPartido.agregarCandidato(unCandidato);
+		otroPartido.agregarCandidato(otroCandidato);
+		otroPartido.agregarCandidato(otroCandidatoFR);
+		unaProvincia.agregarPartido(unPartido);
+		unaProvincia.agregarPartido(otroPartido);
+		centroDeComputos.agregarProvincia(unaProvincia);
+		centroDeComputos.votar(unVoto);
+		centroDeComputos.votar(otroVotoMas);
+		centroDeComputos.votar(unVoto);
+		centroDeComputos.votar(unVoto);
+		centroDeComputos.votar(otroVoto);
+
+		Assert.assertEquals(unCandidato, centroDeComputos.getCandidatoConMasVotosANivelNacional());
+	}
+
 }
