a5101cebcda379a84132d39c010a1c4665b01586
Gonza <g.d.escobar88@gmail.com>
2016-04-09 20:14:03 -0300

    Nueva funcionalidad: se agrega precio de libros que extiende de productos

--- ejercicio_libreria/src/test/java/ar/edu/untref/aydoo/BookTest.java
+++ ejercicio_libreria/src/test/java/ar/edu/untref/aydoo/BookTest.java
@@ -7,11 +7,21 @@ public class BookTest {
 
 	@Test
 	public void bookName() {
-		Book book = new Book("Jobs");
+		Book book = new Book("The Hobbit");
 
 		String name = book.getName();
 
-		Assert.assertEquals("Jobs", name);
+		Assert.assertEquals("The Hobbit", name);
+	}
+	
+	@Test
+	public void bookPrice() {
+		Book book = new Book("The Hobbit");
+		
+		book.setPrice(50);
+		double price = book.getPrice();
+
+		Assert.assertEquals(50, price, 0.1);
 	}
 
 }
