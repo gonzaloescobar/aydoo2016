4cc97147ebbc7104f23ffc5cf163674912c0fe88
Gonza <g.d.escobar88@gmail.com>
2016-04-18 21:57:34 -0300

    Se cambia Set por Array de String

--- factores-primos-3/src/main/java/ar/edu/untref/aydoo/ControladorDeEntrada.java
+++ factores-primos-3/src/main/java/ar/edu/untref/aydoo/ControladorDeEntrada.java
@@ -1,21 +1,15 @@
 package ar.edu.untref.aydoo;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class ControladorDeEntrada {
 	private int numero = 0;
 	private String formato = "pretty";
 	private String output = "";
 	private String orden = "";
-	private Set<String> argumentos;
+	private String argumentos[];
 
 	public ControladorDeEntrada(String args[]) {
 		this.numero = Integer.parseInt(args[0]);
-		this.argumentos = new HashSet<String>();
-		for (String argumentoActual : args) {
-			this.argumentos.add(argumentoActual);
-		}
+		this.argumentos = args;
 	}
 
 	public void analizarArgumentos() {
